7ac4429ec53c198002bfdf0bbbf27a4e923d0db7
Ivan <fenomen_ny@mail.ru>
2023-05-16 23:24:28 +0300

    7 ДЗ дизайн готов

--- src/s2-homeworks/hw07/HW7.tsx
+++ src/s2-homeworks/hw07/HW7.tsx
@@ -17,9 +17,9 @@ export type ArrType = {
 }
 
 const arr: ArrType[] = [
-    { id: '1', value: 'x' },
-    { id: '2', value: 'y' },
-    { id: '3', value: 'z' },
+    { id: '1', value: 'Pre-junior' },
+    { id: '2', value: 'Junior' },
+    { id: '3', value: 'Junior +' },
 ] // value может быть изменено
 
 const HW7 = () => {
