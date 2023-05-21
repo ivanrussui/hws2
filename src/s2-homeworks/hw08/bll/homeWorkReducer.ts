8c6bb83096201294d9e18815069f113f1d7a04d6
Ivan <fenomen_ny@mail.ru>
2023-05-22 00:07:25 +0300

    8 ДЗ фикс copy state

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -8,13 +8,13 @@ export const homeWorkReducer = (state: UserType[], action: ActionType): UserType
     switch (action.type) {
         case 'sort': { // by name
             if (action.payload === 'up') {
-                return [...state.sort((a, b) => a.name.localeCompare(b.name))]
+                return [...state].sort((a, b) => a.name.localeCompare(b.name))
             } else {
-                return [...state.sort((a, b) => b.name.localeCompare(a.name))]
+                return [...state].sort((a, b) => b.name.localeCompare(a.name))
             }
         }
         case 'check': {
-            return [...state].filter(el => el.age >= 18)
+            return state.filter(el => el.age >= 18)
         }
         default:
             return state
