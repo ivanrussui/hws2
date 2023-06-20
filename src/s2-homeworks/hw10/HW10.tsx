13bbb2287a7350ffe41ae10e80ef3680043a110e
Ivan <fenomen_ny@mail.ru>
2023-06-20 23:30:26 +0300

    11 part 1

--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -21,7 +21,7 @@ const HW10 = () => {
     // useSelector, useDispatch // пишет студент
     const isLoading = useSelector<AppStoreType>(state => state.loading.isLoading)
     const dispatch = useDispatch()
-    console.log(isLoading)
+
     const setLoading = () => { // пишет студент // показать крутилку на 1,5 секунд
         // dispatch
         const action = loadingAC(true)
--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { ChangeEvent, useState } from 'react'
 import s from './HW11.module.css'
 import s2 from '../../s1-main/App.module.css'
 import { restoreState } from '../hw06/localStorage/localStorage'
@@ -15,8 +15,20 @@ function HW11() {
     const [value1, setValue1] = useState(restoreState<number>('hw11-value1', 0))
     const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100))
 
-    const change = (event: any, value: any) => {
+    const change = (e: Event, value: number | number[], activeThumb: number) => {
         // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
+        if (activeThumb === 1) {
+            // setValue1(value)
+            // console.log(value1)
+        }
+        // if (Array.isArray(value)) {
+        //         setValue1(e.currentTarget.value)
+        //         setValue2(value2)
+        // } else {
+        // }
+        // let newValue: number = value
+        // setValue1(newValue)
+        // console.log(typeof newValue)
     }
 
     return (
@@ -30,7 +42,7 @@ function HW11() {
                         <SuperRange
                             id={'hw11-single-slider'}
                             // сделать так чтоб value1 изменялось // пишет студент
-
+                            onChange={(e: Event)=>change(e, value1, 1)}
                         />
                     </div>
                     <div className={s.wrapper}>
@@ -38,7 +50,7 @@ function HW11() {
                         <SuperRange
                             id={'hw11-double-slider'}
                             // сделать так чтоб value1/2 изменялось // пишет студент
-
+                            onChange={(e) => change(e, [value1, value2], 2)}
                         />
                         <span id={'hw11-value-2'} className={s.number}>{value2}</span>
                     </div>
